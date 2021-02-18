3df692fa16e98ea8ca86eade4cca0e691ba5a100
Kerry Lang <klang812@gmail.com>
2021-02-17 16:55:50 -0800

    add clear button
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>
    Co-authored by: Nick Lindau <nicholaithegreat@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,22 +7,22 @@ import OpenCageService from './services/open-cage-service.js';
 
 function checkForecastMain(forecastMain) {
   if (forecastMain === "Rain") {
-    $(`.new-${count}`).append('<img src="../assets/images/rain.png" alt="rain" height="50px"><br> ______________________<br><br>');
+    $(`.new-${count}`).append('<img src="../assets/images/rain.png" alt="rain" height="50px"><br> <span class="center">______________________</span><br><br>');
   }
   else if (forecastMain === "Drizzle") {
-    $(`.new-${count}`).append('<img src="../assets/images/drizzle.png" alt="drizzle" height="50px"><br>______________________<br><br>');
+    $(`.new-${count}`).append('<img src="../assets/images/drizzle.png" alt="drizzle" height="50px"><br><span class="center">______________________</span><br><br>');
   }
   else if (forecastMain === "Clear") {
-    $(`.new-${count}`).append('<img src="../assets/images/sunny.png" alt="sunny" height="50px"><br>______________________<br><br>');
+    $(`.new-${count}`).append('<img src="../assets/images/sunny.png" alt="sunny" height="50px"><br><span class="center">______________________</span><br><br>');
   }
   else if (forecastMain === "Snow") {
-    $(`.new-${count}`).append('<img src="../assets/images/snow.png" alt="snow" height="50px"><br>______________________<br><br>');
+    $(`.new-${count}`).append('<img src="../assets/images/snow.png" alt="snow" height="50px"><br><span class="center">______________________</span><br><br>');
   }
   else if (forecastMain === "Thunderstorm") {
-    $(`.new-${count}`).append('<img src="../assets/images/lightning.png" alt="thunderstorm." height="50px"><br>______________________<br><br>');
+    $(`.new-${count}`).append('<img src="../assets/images/lightning.png" alt="thunderstorm." height="50px"><br><span class="center">______________________</span><br><br>');
   }
   else if (forecastMain === "Clouds") {
-    $(`.new-${count}`).append('<img src="../assets/images/cloudy.png" alt="clouds" height="50px"><br>______________________<br><br>');
+    $(`.new-${count}`).append('<img src="../assets/images/cloudy.png" alt="clouds" height="50px"><br><span class="center">______________________</span><br><br>');
   }
 }
 
@@ -38,25 +38,30 @@ function createDiv(cityName) {
 }
 
 $(document).ready(function () {
+  var buttonObj = document.querySelector("button");
+  $('#restart').click(() => {
+    document.location.reload(true);       //Reload Page
+    buttonObj.textContent = "submit";
+  });
   $(".location-form").submit(function (event) {
+    var buttonObj = document.querySelector("button");
     event.preventDefault();
     count += 1;
     if (count <= 4) {
       let cityName = ($('#location').val()).toUpperCase();
       $('#location').val('');
       createDiv(cityName);
-      console.log(count);
+      //console.log(count);
       OpenCageService.getCoord(cityName)
         .then(async function (coordResponse) {
-          if (coordResponse instanceof Error) {
-            // throw Error();
-          }
+          // if (coordResponse instanceof Error) {
+          // }
           let weatherForecastResponse = await OpenWeatherService.getWeatherForecast(coordResponse.results[0].geometry.lat, coordResponse.results[0].geometry.lng);
-          console.log(weatherForecastResponse);
+          //console.log(weatherForecastResponse);
           for (let i = 0; i < weatherForecastResponse.daily.length; i++) {
             let unixTimestamp = weatherForecastResponse.daily[i].dt;
             let date = [];
-            console.log(date);
+            //console.log(date);
             for (let i = 0; i < 4; i++) {
               date.push(`${new Date(unixTimestamp * 1000).toDateString().split(' ')[i]}`);
             }
@@ -68,7 +73,6 @@ $(document).ready(function () {
             let sunset = weatherForecastResponse.daily[i].sunset;
             let sunsetTime = (new Date(sunset * 1000).toString().split(' ')[4]);
 
-            // let temp = weatherForecastResponse.daily[i].temp.day;
             let temp = await (weatherForecastResponse.daily[i].temp.day);
             let convertTemp = Math.round(tempConversion(temp));
             let forecast = await weatherForecastResponse.daily[i].weather[0].description;
@@ -79,22 +83,31 @@ $(document).ready(function () {
             $(`.new-${count}`).append(`<span class ="key">Sunrise:</span> ${sunriseTime}<br>`);
             $(`.new-${count}`).append(`<span class ="key"> Sunset:</span> ${sunsetTime}<br>`);
             $(`.new-${count}`).append(`<span class ="key">Forecast:</span> ${forecast}<br>`);
-            console.log(forecast);
-            console.log(forecastMain);
+            //console.log(forecast);
+            //console.log(forecastMain);
             checkForecastMain(forecastMain);
           }
-          $(".jumbotron").slideDown();
+          $(".jumbotron").slideDown("slow");
+          $(`.new-${count}`).slideDown("slow");
+          if (count === 4) {
+            buttonObj.disabled = true;
+            buttonObj.textContent = "Submitted";
+          }
+          else {
+            // Do nothing
+          }
         })
         .catch(function () {
           $("#invalidCity").empty();
           $("#invalidCity").append("Please enter a valid location.");
           $(`.new-${count}`).remove();
           count -= 1;
-          console.log(count);
+          //console.log(count);
         });
     }
     else {
       $('#city-limit').text("You have reached the city search limit.");
+
     }
   });
 });
